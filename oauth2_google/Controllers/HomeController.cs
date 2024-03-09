f507ac2e289db12aacc314635211210297eb8bf1
omelenko <vasul.melenko@gmail.com>
2024-03-09 20:38:39 +0300

    lozalization

--- oauth2_google/Controllers/HomeController.cs
+++ oauth2_google/Controllers/HomeController.cs
@@ -1,25 +1,54 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using oauth2_google.Models;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace oauth2_google.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IStringLocalizer<HomeController> _localizer;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IStringLocalizer<HomeController> localizer)
         {
             _logger = logger;
+            _localizer = localizer;
         }
 
         public IActionResult Index()
         {
+            ViewData["Greeting"] = _localizer["Greeting"];
+            ViewData["Greeting1"] = _localizer["Greeting1"];
+            ViewData["Greeting2"] = _localizer["Greeting2"];
+            ViewData["Home"] = _localizer["Home"];
+            ViewData["Login"] = _localizer["Login"];
+            ViewData["Register"] = _localizer["Register"];
+            ViewData["Privacy"] = _localizer["Privacy"];
+            ViewData["Privacy1"] = _localizer["Privacy1"];
+            ViewData["Hello"] = _localizer["Hello"];
+            ViewData["Logout"] = _localizer["Logout"];
+            ViewData["Privacy2"] = _localizer["Privacy2"];
+            /*ViewData[""] = _localizer[""];
+            ViewData[""] = _localizer[""];
+            ViewData[""] = _localizer[""];*/
             return View();
         }
 
         public IActionResult Privacy()
         {
+            ViewData["Greeting"] = _localizer["Greeting"];
+            ViewData["Greeting1"] = _localizer["Greeting1"];
+            ViewData["Greeting2"] = _localizer["Greeting2"];
+            ViewData["Home"] = _localizer["Home"];
+            ViewData["Login"] = _localizer["Login"];
+            ViewData["Register"] = _localizer["Register"];
+            ViewData["Privacy"] = _localizer["Privacy"];
+            ViewData["Privacy1"] = _localizer["Privacy1"];
+            ViewData["Hello"] = _localizer["Hello"];
+            ViewData["Logout"] = _localizer["Logout"];
+            ViewData["Privacy2"] = _localizer["Privacy2"];
             return View();
         }
 
--- oauth2_google/Program.cs
+++ oauth2_google/Program.cs
@@ -1,6 +1,13 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using MimeKit;
 using oauth2_google.Data;
+using System;
+using MailKit.Net.Smtp;
+using MailKit;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,9 +23,21 @@ builder.Services.AddAuthentication().AddGoogle(googleOptions =>
     googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
 });
 
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+const string defaultCulture = "en-GB";
+var supportedCultures = new[]
+{
+    new CultureInfo(defaultCulture),
+    new CultureInfo("uk-UA")
+};
+builder.Services.Configure<RequestLocalizationOptions>(options => {
+    options.DefaultRequestCulture = new RequestCulture(defaultCulture);
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(); 
 
 var app = builder.Build();
 
@@ -37,7 +56,27 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseRequestLocalization(app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
+/*var email = new MimeMessage();
 
+email.From.Add(new MailboxAddress("Sender Name", "sender@email.com"));
+email.To.Add(new MailboxAddress("Receiver Name", "receiver@email.com"));
+
+email.Subject = "Testing out email sending";
+email.Body = new TextPart(MimeKit.Text.TextFormat.Plain)
+{
+    Text = "Hello all the way from the land of C#"
+};
+using (var smtp = new SmtpClient())
+{
+    smtp.Connect("smtp.gmail.com", 587, false);
+
+    // Note: only needed if the SMTP server requires authentication
+    smtp.Authenticate("smtp_username", "smtp_password");
+
+    smtp.Send(email);
+    smtp.Disconnect(true);
+}*/
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
